b5f7c2441eacc49dc321e22ee5773eac70d2f9a7
Matheus Mendes <matheusmendesreis@outlook>
2023-12-02 08:53:40 -0300

    feat:criacao do db

--- src/main/java/com/example/app/modules/candidate/CandidateEntity.java
+++ src/main/java/com/example/app/modules/candidate/CandidateEntity.java
@@ -1,25 +1,30 @@
 package com.example.app.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
-
+@Entity(name = "candidate")
 public class CandidateEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
   private String name;
-  @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaço")
+  @NotBlank
+  @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
   private String userName;
   @Email(message = "O campo deve conter uma email válido.")
   private String email;
@@ -27,5 +32,8 @@ public class CandidateEntity {
   private String password;
   private String description;
   private String curriculum;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 
 }
