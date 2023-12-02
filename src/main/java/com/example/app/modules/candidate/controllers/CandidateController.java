2063e25df6355f0ac02d4351cf77ae64867d3f01
Matheus Mendes <matheusmendesreis@outlook>
2023-12-02 07:56:27 -0300

    personalizando menssagem de erro

--- src/main/java/com/example/app/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/example/app/modules/candidate/controllers/CandidateController.java
@@ -1,6 +1,7 @@
 package com.example.app.modules.candidate.controllers;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ import jakarta.validation.Valid;
 public class CandidateController {
 
   @PostMapping("/")
-  public void created(@Valid CandidateEntity candidateEntity){
+  public void created(@RequestBody  @Valid CandidateEntity candidateEntity){
     System.out.println("Candidato");
     System.out.println(candidateEntity.getEmail());  
   }
---
+++ src/main/java/com/example/app/modules/exceptions/ErroMessageDTO.java
@@ -0,0 +1,11 @@
+package com.example.app.modules.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErroMessageDTO {
+  private String message;
+  private String field;
+}
