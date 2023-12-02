fa9581dbc3217a4588df249ea6feaf1ed6d594cd
Matheus Mendes <matheusmendesreis@outlook>
2023-12-02 08:33:24 -0300

    adicionei p genareted value

--- src/main/java/com/example/app/modules/candidate/CandidateEntity.java
+++ src/main/java/com/example/app/modules/candidate/CandidateEntity.java
@@ -4,6 +4,8 @@ import java.util.UUID;
 
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
@@ -14,6 +16,7 @@ import lombok.Data;
 public class CandidateEntity {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
   private String name;
   @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaço")
