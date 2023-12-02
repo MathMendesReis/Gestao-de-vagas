e4a7aa44989a6e88e822b702f234cd9154eed6e7
Matheus Mendes <matheusmendesreis@outlook>
2023-12-02 08:32:41 -0300

    adicionei a anaotation id

--- src/main/java/com/example/app/modules/candidate/CandidateEntity.java
+++ src/main/java/com/example/app/modules/candidate/CandidateEntity.java
@@ -4,12 +4,16 @@ import java.util.UUID;
 
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+
 public class CandidateEntity {
+
+  @Id
   private UUID id;
   private String name;
   @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaço")
